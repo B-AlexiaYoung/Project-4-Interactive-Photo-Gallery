beac59a920af456422003b56cae39dc14a6da218
B.Alexia Young <brlxyng@gmail.com>
2018-03-07 17:23:29 -0700

    initial files

---
+++ js/jsInteractivePhoto.js
@@ -0,0 +1,54 @@
+ // javascript Interactive Photo gallery //
+
+ lightbox.option({
+    alwaysShowNavOnTouchDevices: true,
+    wrapAround: true
+  });
+
+
+$("#search").keyup(function(){ 
+
+  let searchTerm = $('#search').val();
+  searchTerm = searchTerm.toLowerCase();
+  //console.log(searchTerm);
+  let match = document.querySelectorAll('a[data-title]');
+  //console.log(match);
+  for (i=0; i< match.length; i++){
+
+  //console.log(match[i].getAttribute('data-title'));
+  if  (match[i].getAttribute('data-title').toLowerCase().includes(searchTerm)){
+  match [i].style.display="inline";
+  } else {
+  match [i].style.display="none";
+  }
+
+  };
+
+
+})
+
+     
+      
+
+    
+
+
+
+
+
+  
+//   I'm stuck!!
+//for(i=0; i=photoList.length; i++){
+            
+            
+            
+            //let match = getElementsByTagName('a')[i].getAttribute('data-title');
+          
+                //if (match.includes(searchTerm)){
+                 //   display: li[i];
+               // } else  {
+                //    hide:li[i];
+                //}
+
+            
+//})
--- jsInteractivePhoto.js
+++
@@ -1,1 +0,0 @@
- // javascript Interactive Photo gallery //
